0df048782a5c85ec11b138eb54def34bd26ce5a0
Alia Haven <aliahavenart@gmail.com>
2022-03-05 17:18:56 +0000

    added some styling and updated Readme file

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ function addMember() {
         type: "list",
         message: "Select team member's role",
         choices: [
+            "manager",
             "engineer",
-            "intern",
-            "manager"
+            "intern"
         ],
         name: "role"
     },
@@ -87,10 +87,12 @@ function startHtml() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="../src/stylesheet.css">
         <title>Team Profile</title>
     </head>
     <body>
-        <nav class="navbar navbar-dark bg-dark mb-5">
+        <nav class="navbar navbar-dark bg-dark mb-5 shadow-sm" id="navigation">
             <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
         </nav>
         <div class="container">
@@ -113,36 +115,39 @@ function addHtml(member) {
         if (role === "engineer") {
             const gitHub = member.getGithub();
             data = `<div class="col-4">
-            <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Engineer</h5>
+            <div class="card mx-auto mb-3 shadow" style="width: 18rem" id="profile-card">
+            <h4 class="card-header" id="name-header">${name}</h4>
+            <h6 class="card-header" id="role-header"><i class="bi bi-stack"></i> Engineer</h6>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">GitHub: ${gitHub}</li>
+                <li class="list-group-item"><i class="bi bi-person" id="list-icon"></i> ID: ${id}</li>
+                <li class="list-group-item"><i class="bi bi-envelope" id="list-icon"></i> Email: ${email}</li>
+                <li class="list-group-item"><i class="bi bi-github" id="list-icon"></i> GitHub: ${gitHub}</li>
             </ul>
             </div>
         </div>`;
         } else if (role === "intern") {
             const school = member.getSchool();
             data = `<div class="col-4">
-            <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Intern</h5>
+            <div class="card mx-auto mb-3 shadow" style="width: 18rem" id="profile-card">
+            <h4 class="card-header" id="name-header">${name}</h4>
+            <h6 class="card-header" id="role-header"><i class="bi bi-mortarboard-fill"></i> Intern</h6>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">School: ${school}</li>
+                <li class="list-group-item"><i class="bi bi-person" id="list-icon"></i> ID: ${id}</li>
+                <li class="list-group-item"><i class="bi bi-envelope" id="list-icon"></i> Email: ${email}</li>
+                <li class="list-group-item"><i class="bi bi-building" id="list-icon"></i> School: ${school}</li>
             </ul>
             </div>
         </div>`;
         } else {
             const officePhone = member.getOfficeNumber();
             data = `<div class="col-4">
-            <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Manager</h5>
+            <div class="card mx-auto mb-3 shadow" style="width: 18rem" id="profile-card">
+            <h4 class="card-header" id="name-header">${name}</h4>
+            <h6 class="card-header" id="role-header"><i class="bi bi-briefcase-fill"></i> Manager</h6>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">Office Phone: ${officePhone}</li>
+                <li class="list-group-item"><i class="bi bi-person" id="list-icon"></i> ID: ${id}</li>
+                <li class="list-group-item"><i class="bi bi-envelope" id="list-icon"></i> Email: ${email}</li>
+                <li class="list-group-item"><i class="bi bi-telephone" id="list-icon"></i> Office Phone: ${officePhone}</li>
             </ul>
             </div>
         </div>`
@@ -155,12 +160,6 @@ function addHtml(member) {
             return resolve();
         });
     });
-
-
-
-
-
-
 }
 
 function finishHtml() {
@@ -175,7 +174,7 @@ function finishHtml() {
             console.log(err);
         };
     });
-    console.log("end");
+    console.log("Your file has been generated, please find it in the dist folder");
 }
 
 initApp();
